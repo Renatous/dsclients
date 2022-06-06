01d74656718156a66d5519784cb04cae973cb803
Renato Teixeira <renato.teixeira.93@gmail.com>
2022-06-06 09:36:22 +0100

    H2 database

--- backend/src/main/java/com/devsuperior/dsclients/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclients/resources/ClientResource.java
@@ -1,23 +1,26 @@
 package com.devsuperior.dsclients.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclients.entities.Client;
+import com.devsuperior.dsclients.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 
+	@Autowired
+	private ClientService service;
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll() {
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Maria Silva", "12345678901", 6500.0, 2));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
---
+++ backend/src/main/java/com/devsuperior/dsclients/services/ClientService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dsclients.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsclients.entities.Client;
+import com.devsuperior.dsclients.repositories.ClientRepository;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+	
+	public List<Client> findAll() {
+		return repository.findAll();
+	}
+}
